39a2daa8929acc40b36dfc0dcf9d5f2d152eab64
ronesta <gvozzzd123@gmail.com>
2024-12-29 18:02:06 +0300

    Initial commit

---
+++ RMYapDatabaseMVC/RMYapDatabaseMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMYapDatabaseMVC
+//
+//  Created by Ибрагим Габибли on 29.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
